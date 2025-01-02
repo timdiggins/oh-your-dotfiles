b90a4987ecb145a8f8d76caba27e6e97a91e5c95
Tim Diggins <tim@red56.uk>
2025-01-02 11:52:12 +0000

    drop custom debugging

--- lib/git.zsh
+++ lib/git.zsh
@@ -1,5 +1,4 @@
 git_clone_or_pull() {
-  echo "   [[[ git_clone_or_pull $1 $2 ]]]"
   if [ -d $2 ]; then
     git_pull $1 $2
   else
@@ -29,7 +28,6 @@ function git_pull() {
   push=$(head -2 $1| tail -1)
   dest=$2
 
-  echo "   [[[ git -C $dest remote set-url origin $fetch ]]]"
   git -C "$dest" remote set-url origin "$fetch"
   if [ "$fetch" != "$push" ]; then
     git -C "$dest" remote set-url origin --push $push
